5aca2ea32e25a6d75bf7442b3fafaf23e16ac2e6
Sebastian Kippe <sebastian@kip.pe>
2019-01-22 21:22:31 +0800

    Build production from 34f7880

--- sw-registration.js
+++ sw-registration.js
@@ -1,7 +1,2 @@
-(function(){"use strict"
-var e=[],r=[]
-"serviceWorker"in navigator&&navigator.serviceWorker.register("/sw.js",{scope:"/"}).then(function(r){for(var n=Promise.resolve(),o=0;o<e.length;o++)(function(o){n=n.then(function(){return e[o](r)})})(o)
-return n.then(function(){console.log("Service Worker registration succeeded. Scope is "+r.scope)})}).catch(function(e){for(var n=Promise.resolve(),o=0;o<r.length;o++)(function(o){n=n.then(function(){return r[o](e)})})(o)
-return n.then(function(){console.log("Service Worker registration failed with "+e)})})})()
-
+!function(){"use strict";var e=[],r=[];"serviceWorker"in navigator&&navigator.serviceWorker.register("/sw.js",{scope:"/"}).then(function(r){for(var n=Promise.resolve(),o=function(o,t){n=n.then(function(){return e[o](r)})},t=0,i=e.length;t<i;t++)o(t);return n.then(function(){console.log("Service Worker registration succeeded. Scope is "+r.scope)})}).catch(function(e){for(var n=Promise.resolve(),o=function(o,t){n=n.then(function(){return r[o](e)})},t=0,i=r.length;t<i;t++)o(t);return n.then(function(){console.log("Service Worker registration failed with "+e)})})}();
 //# sourceMappingURL=sw-registration.map
